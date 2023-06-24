3c9387a4698571bff139f6f93c4db3b272c4d5bd
Restyled.io <commits@restyled.io>
2023-06-24 06:48:20 +0000

    Restyled by autopep8

--- maniskill/label_objects.py
+++ maniskill/label_objects.py
@@ -5,13 +5,14 @@ from PIL import Image
 import os
 import yaml
 
+
 def label_interactive(path: str, overwrite: bool = False):
     """
      finding similarity between a descriptor in one image to the all descriptors in the other image.
      :param path: path to image directory.
      :param stride: stride of the model.
     """
-    
+
     # plot
     if os.path.exists(path):
         for task_dir in os.listdir(path):
@@ -36,32 +37,37 @@ def label_interactive(path: str, overwrite: bool = False):
                                 continue
                             else:
                                 print("Overwriting...")
-                                
-                    pil_image = Image.open(os.path.join(path, task_dir, image_file)).convert('RGB')
-                    
+
+                    pil_image = Image.open(os.path.join(
+                        path, task_dir, image_file)).convert('RGB')
+
                     radius = 1
                     # plot image
                     axes.imshow(pil_image)
                     # get input point from user
-                    fig.suptitle('Select points on the image (LClick). Next Image (RClick)', fontsize=16)
+                    fig.suptitle(
+                        'Select points on the image (LClick). Next Image (RClick)', fontsize=16)
                     # reset previous marks
                     for patch in visible_patches:
                         patch.remove()
                         visible_patches = []
-                    
+
                     if image_file in current_info:
                         if len(current_info[image_file]['points']) > 0:
                             for point in current_info[image_file]['points']:
-                                y_pt, x_pt = map(lambda x: float(x.strip()), point.split(' '))
+                                y_pt, x_pt = map(lambda x: float(
+                                    x.strip()), point.split(' '))
                                 center = (x_pt - 0.5, y_pt - 0.5)
-                                patch = plt.Circle(center, radius, color=(1, 0, 0, 0.75))
+                                patch = plt.Circle(
+                                    center, radius, color=(1, 0, 0, 0.75))
                                 axes.add_patch(patch)
                                 visible_patches.append(patch)
-                        
+
                     plt.draw()
-                    pts = np.asarray(plt.ginput(n=-1, timeout=-1, show_clicks=True, mouse_stop=plt.MouseButton.RIGHT, mouse_pop=plt.MouseButton.MIDDLE))
+                    pts = np.asarray(plt.ginput(n=-1, timeout=-1, show_clicks=True,
+                                     mouse_stop=plt.MouseButton.RIGHT, mouse_pop=plt.MouseButton.MIDDLE))
                     print(pts)
-                    
+
                     if len(pts) > 0:
                         points = [str(pt)[1:-1].strip() for pt in pts]
                         current_info[image_file] = {
@@ -69,17 +75,18 @@ def label_interactive(path: str, overwrite: bool = False):
                             'points': points
                         }
                         stream = open(labels_file, 'w')
-                        yaml.dump(current_info, stream) 
+                        yaml.dump(current_info, stream)
                         stream.close()
-                        
+
                         for pt in pts:
                             print("Selected point: (%f, %f)" % (pt[1], pt[0]))
                             y_coor, x_coor = int(pt[1]), int(pt[0])
-                        
+
                             # draw chosen point
                             center = (x_coor - 0.5, y_coor - 0.5)
-                            
-                            patch = plt.Circle(center, radius, color=(1, 0, 0, 0.75))
+
+                            patch = plt.Circle(
+                                center, radius, color=(1, 0, 0, 0.75))
                             axes.add_patch(patch)
                             visible_patches.append(patch)
                         plt.draw()
@@ -87,7 +94,10 @@ def label_interactive(path: str, overwrite: bool = False):
         print('Data directory does not exist. Exiting...')
         return
 
+
 """ taken from https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse"""
+
+
 def str2bool(v):
     if isinstance(v, bool):
         return v
@@ -98,11 +108,15 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Label the objects in the training set of images.')
-    parser.add_argument('--path', type=str, required=True, help='Path to the dataset directory')
-    parser.add_argument('--overwrite', default='False', type=str2bool, help="Overwrite the existing label data.")
-    
+    parser = argparse.ArgumentParser(
+        description='Label the objects in the training set of images.')
+    parser.add_argument('--path', type=str, required=True,
+                        help='Path to the dataset directory')
+    parser.add_argument('--overwrite', default='False',
+                        type=str2bool, help="Overwrite the existing label data.")
+
     args = parser.parse_args()
-    
+
     label_interactive(args.path, args.overwrite)
