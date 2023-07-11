ba8171d032269285736199893f89621997bc6ab6
peter.koenigstein <pkonigstein@gmail.com>
2023-07-11 03:22:44 +0200

    More possibilities of extracting descriptors

--- maniskill/extract_descriptors.py
+++ maniskill/extract_descriptors.py
@@ -2,7 +2,7 @@ import sys
 
 import os
 from typing import List
-
+import random
 from torchmetrics.functional import pairwise_manhattan_distance
 import numpy as np
 import torch.utils.data
@@ -53,29 +53,66 @@ def get_descriptors(image_path: str):
         return descs.detach()
 
 
-if __name__ == '__main__':
-    training_data_path = f"training_data/training_set"
+def read_points(task_folder: str):
+    with open(os.path.join(task_folder, "labels.yml"), "r") as label_file:
+        points = []
+        files = []
+        try:
+            labels = yaml.safe_load(label_file)
+            current_descriptors = torch.tensor([], device=device)
+            for key, value in labels.items():
+                points.extend(value["points"])
+                files.extend([key] * len(value["points"]))
+        except yaml.YAMLError as exc:
+            print(exc)
+        return [files, points]
 
+
+def extract_descriptors(tasks: List[str], descriptor_amount: int = None):
     # in all_descriptors all the descriptors for the objects will be safed
-    all_descriptors = torch.tensor([], device= device)
-    descriptor_labels = []
-    tasks = os.listdir(training_data_path)
+    descriptors = []
     tasks.sort()
     for task in tasks:
         task_folder = os.path.join(training_data_path, task)
-        labels = None
-        with open(os.path.join(task_folder, "labels.yml"), "r") as label_file:
-            print(task)
-            try:
-                labels = yaml.safe_load(label_file)
-                current_descriptors = torch.tensor([], device=device)
-                for key, value in labels.items():
-                    for point_pair in value["points"]:
-                        descriptor = get_descriptor_for_labeled(os.path.join(os.path.join(task_folder, key)), point_pair).detach()
-                        current_descriptors = torch.cat([current_descriptors, descriptor])
-                all_descriptors = torch.cat([all_descriptors, current_descriptors])
-                descriptor_labels.extend([task] * current_descriptors.shape[0])
-            except yaml.YAMLError as exc:
-                print(exc)
-    pkl.dump(all_descriptors, open(f"training_data/descriptors.pkl", "wb"))
-    pkl.dump(descriptor_labels, open(f"training_data/descriptor_labels.pkl","wb"))
+        [files, points] = read_points(task_folder)
+        current_descriptors = torch.tensor([], device=device)
+        zipped = list(zip(files, points))
+        zip_idx = list(range(0, len(zipped)))
+        random.shuffle(zip_idx)
+        max_idx = len(zipped)
+        if descriptor_amount is not None:
+            max_idx = min(max_idx, descriptor_amount)
+        for key_idx in range(max_idx):
+            [file, point] = zipped[zip_idx[key_idx]]
+            descriptor = get_descriptor_for_labeled(os.path.join(os.path.join(task_folder, file)),
+                                                    point).detach()
+            current_descriptors = torch.cat([current_descriptors, descriptor])
+        descriptors.append({
+            "object": task,
+            "descriptors": torch.transpose(current_descriptors,0,2)
+        })
+    return descriptors
+
+
+def extract_topk_descr(descr: List[dict], method: str, k: int = 5):
+    assert method in ["similar", "dissimilar"]
+    for task in descr:
+        curr_descriptor = task["descriptors"]
+        similarities = chunk_cosine_sim(curr_descriptor, curr_descriptor)
+        summed_similarities = torch.sum(similarities, dim=3)
+        if method == "similar":
+            largest = True
+        else:
+            largest = False
+        top_k_descriptors = torch.topk(summed_similarities, k, largest=largest)
+        topk_indices = top_k_descriptors.indices.squeeze()
+        new_descriptors = torch.index_select(curr_descriptor, 2, topk_indices)
+        task["descriptors"] = new_descriptors
+    return descr
+
+
+if __name__ == '__main__':
+    training_data_path = f"training_data/training_set"
+    descriptors = extract_descriptors(tasks=os.listdir(training_data_path))
+    descriptors = extract_topk_descr(descriptors, "similar")
+    pkl.dump(descriptors, open(f"training_data/descriptor_data.pkl", "wb"))
