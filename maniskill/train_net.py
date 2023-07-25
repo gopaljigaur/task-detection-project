eb743f00ebecd68c5b5c8af3328be10087163def
Anurag garg <garg12099@gmail.com>
2023-07-25 13:49:01 +0000

    update train_net

--- maniskill/train_net.py
+++ maniskill/train_net.py
@@ -5,10 +5,10 @@ import torch
 from torch.utils.data import DataLoader
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-net = TaskClassifier(vit_stride=4)
+net = TaskClassifier(vit_stride=4).to(device)
 
 criterion = nn.CrossEntropyLoss()
-optim = optim.Adam(net.parameters(), lr=0.001)
+optim = optim.Adam(net.parameters(), lr=0.0001)
 image_path = "training_data/training_set"
 net.preprocess(image_path)
 tensor_data_set = net.load_cache("training_data/training_set")[1]
@@ -26,5 +26,7 @@ for epoch in range(num_epochs):
         optim.zero_grad()
         loss.backward()
         optim.step()
-
+    if epoch >10 and epoch % 5 ==0:
+        torch.save(net, f"model_{epoch}.pth")
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+
