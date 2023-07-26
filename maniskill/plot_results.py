4858861e8f772f8247de85218b8d9be85b82e6e6
peter.koenigstein <pkonigstein@gmail.com>
2023-07-26 02:26:15 +0200

    plot image

---
+++ maniskill/plot_results.py
@@ -0,0 +1,35 @@
+import torch
+import matplotlib.image as implt
+import matplotlib.pyplot as plt
+
+from maniskill.task_classifier import TaskClassifier
+
+
+def load_img_tensor(img_path:str):
+    return torch.load(img_path.split(".")[0]+".pt")
+
+def load_img(img_path: str):
+    return implt.imread(img_path.split(".")[0]+".png","rgbd")
+
+def plot_img(img_path:str):
+    im_tensor = load_img_tensor(img_path)
+    img = load_img(img_path)
+    net = TaskClassifier(vit_stride=4)
+    net.obj_finder.skip_agg = True
+    output = net.obj_finder(im_tensor)
+    reshaped = torch.reshape(output, (5,3))
+    colors = ["red","blue","yellow","purple","green"]
+    fig, ax = plt.subplots()
+    plt.imshow(img)
+    dots = []
+    class_mapping = {v: k for k, v in net.obj_finder.class_mapping.items()}
+    for key, task in class_mapping.items():
+        circle = plt.Circle(reshaped[key][:2], radius=2, color=colors[key], label=f"{task} {round(reshaped[key][2].item()*100,1)}")
+        dots.append(circle)
+        ax.add_patch(circle)
+    plt.legend(handles=dots)
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_img("training_data/training_set_multiple/ReleaseBlock-v0/1690207701091_1.png")
