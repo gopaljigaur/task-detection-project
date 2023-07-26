920fd6170dc968f55b12a1a20264039262540e68
peter.koenigstein <pkonigstein@gmail.com>
2023-07-26 05:22:32 +0200

    added descriptor exploration for multiple object datasets

--- maniskill/compare_descriptors.py
+++ maniskill/compare_descriptors.py
@@ -14,6 +14,8 @@ from maniskill.extract_descriptors import extract_descriptors
 from maniskill.helpers.DescriptorConfiguration import DescriptorConfiguration
 from maniskill.task_classifier import *
 
+multiple_object_tasks = {"CollectTools-v0":["StoreWrench-v0","PickupDrill-v0","Hammer-v0"], "DrillBlock-v0":["PickupDrill-v0","PickUpBlock-v0"], "FastenBlock-v0":["PickUpBlock-v0","FindClamp-v0"], "ReleaseBlock-v0":["PickUpBlock-v0","FindClamp-v0"]}
+
 custom_single_tasks = ["PickupDrill-v0", "PickUpBlock-v0", "FindClamp-v0", "StoreWrench-v0", "Hammer-v0"]
 stride = 4
 patch_size = 8
@@ -93,7 +95,7 @@ def try_configurations_for_tasks(tasks:List[str]):
     for task in tasks:
         try_configuration(task)
 
-def try_configuration(task:str, filter_fn=None, name=".", thresholds=None, k_list=None):
+def try_configuration(task:str, filter_fn=None, name=".", thresholds=None, k_list=None, label_name ="labels.yml"):
     if thresholds is None:
         thresholds = [thr / 100 for thr in range(30, 80, 10)]
     if k_list is None:
@@ -101,13 +103,13 @@ def try_configuration(task:str, filter_fn=None, name=".", thresholds=None, k_lis
     if not name == ".":
         if not os.path.exists(f"training_data/{name}"):
             os.mkdir(f"training_data/{name}")
-    base_path = "training_data/validation_set"
+    base_path = "training_data/validation_multiple"
     results = {}
     print(f"{task} ", end="")
     torch.cuda.empty_cache()
     results[task] = []
     descriptor_sets = []
-    all_desc = extract_descriptors([task])[0]
+    all_desc = extract_descriptors([task],fname=label_name)[0]
     descriptor_sets.append(all_desc)
     for k in k_list:
         sim_set = all_desc.extract_top_k("similar", k)
@@ -120,7 +122,7 @@ def try_configuration(task:str, filter_fn=None, name=".", thresholds=None, k_lis
         for descriptor_set in descriptor_sets:
             torch.cuda.empty_cache()
             configuration = DescriptorConfiguration(descriptor_set, threshold, 0)
-            run_experiment(base_path, task, configuration, filter_fn=filter_fn)
+            run_experiment_multiple(base_path, task, configuration, filter_fn=filter_fn)
             results[task].append(configuration)
         print(threshold, end=" ")
     for descriptor_set in descriptor_sets:
@@ -155,16 +157,16 @@ def extract_top_k_results(result_dict, k=10, sort_fn=lambda conf: conf.f1):
     return (new_res, descriptors)
 
 
-def optimize_configurations(task:str, iterations:int, k:int, performance_metric=lambda conf: (conf.f1,conf.precision), filter_fn= None, name:str ="optim", explore =0.2):
-    thresholds = [thr / 100 for thr in range(20, 100, 30)]
-    # k_list = [8,14,20]
-    k_list = []
+def optimize_configurations(task:str, iterations:int, k:int, performance_metric=lambda conf: (conf.f1,conf.precision), filter_fn= None, name:str ="optim", explore =0.2, label_name="labels.yml"):
+    thresholds = [thr / 100 for thr in range(20, 100, 20)]
+    k_list = [8,14,20]
+    # k_list = []
     top_k = []
     all_res =[]
     for i in range(1,iterations+1):
         print(f"Epoch {i}")
         print(f"Next iter: Thresholds: {thresholds},  k_list: {k_list}")
-        results = try_configuration(task,filter_fn=filter_fn,name=name,thresholds=thresholds, k_list=k_list)[task]
+        results = try_configuration(task,filter_fn=filter_fn,name=name,thresholds=thresholds, k_list=k_list, label_name = label_name)[task]
         sorted_res = sorted(results, reverse=True, key=performance_metric)
         top_k = sorted_res[:k]
         all_res.extend(top_k)
@@ -227,6 +229,34 @@ def run_experiment(img_src: str, task: str, config: DescriptorConfiguration, fil
     config.record_performance(metrics["tp"],metrics["fn"],metrics["fp"],-1,-1,-1)
 
 
+def run_experiment_multiple(img_src: str, task: str, config: DescriptorConfiguration, filter_fn: Callable = None):
+    net = TaskClassifier(vit_stride=4, descriptors={config.descriptor_set.task: config})
+    [class_mapping, dataset] = net.load_cache(img_src, filter_fn=filter_fn)
+    data_loader = DataLoader(dataset=dataset, batch_size=16, shuffle=True)
+    obj_finder = net.obj_finder
+    obj_finder.skip_agg = True
+    # class_mapping = obj_finder.class_mapping
+    pos = torch.tensor([], device=device)
+    neg = torch.tensor([], device=device)
+    good_labels = []
+    for multiple_task, single_tasks in multiple_object_tasks.items():
+        if task in single_tasks:
+            good_labels.append(class_mapping[multiple_task])
+    for [tensors, labels] in data_loader:
+        tensor = obj_finder(tensors).detach()
+        should_be = torch.tensor([False] * tensor.shape[0], device=device)
+        for label in good_labels:
+            current_label = labels == label
+            should_be = torch.logical_or(should_be, current_label.to(device))
+        # should_be = labels == class_mapping[task]
+        thresholds = tensor[:, 2]
+        pos = torch.cat((pos, thresholds[should_be]))
+        neg = torch.cat((neg, thresholds[~should_be]))
+    [metrics, agg] = compute_aggregation_percentage(pos, neg)
+    config.aggregation_percentage = agg
+    config.record_performance(metrics["tp"], metrics["fn"], metrics["fp"], metrics["tn"], -1, -1)
+
+
 def compute_aggregation_percentage(pos_thr: torch.Tensor, neg_thr: torch.Tensor):
     pos = torch.sort(pos_thr).values
     neg = torch.sort(neg_thr).values
@@ -245,29 +275,30 @@ def get_metrics(pos:torch.Tensor, neg: torch.Tensor, agg: float):
     tp = torch.count_nonzero(pos >= agg).item()
     fn = torch.count_nonzero(pos < agg).item()
     fp = torch.count_nonzero(neg >= agg).item()
+    tn = torch.count_nonzero(neg < agg).item()
     prec = tp / (tp + fp)
     rec = tp / (tp + fn)
     f1 = 2 * (prec * rec / (prec + rec))
-    return {"f1": f1, "prec": prec, "rec": rec, "tp":tp, "fp": fp, "fn": fn}
+    return {"f1": f1, "prec": prec, "rec": rec, "tp":tp, "fp": fp, "fn": fn, "tn":tn}
 
 if __name__ == '__main__':
     for i in range(len(custom_single_tasks)):
-        optim_config = optimize_configurations(custom_single_tasks[i],3,6, filter_fn=lambda name:"_1" in name[0], name="hand")
-        pkl.dump(optim_config, open(f"training_data/optim_{custom_single_tasks[i]}_hand.pkl","wb"))
+        optim_config = optimize_configurations(custom_single_tasks[i],4,6, filter_fn=lambda name:"_0" in name[0], name="static")
+        pkl.dump(optim_config, open(f"training_data/optim_{custom_single_tasks[i]}_multi_static.pkl","wb"))
     all_desc = {}
     for i in range(len(custom_single_tasks)):
-        config = pkl.load(open(f"training_data/optim_{custom_single_tasks[i]}_hand.pkl","rb"))
+        config = pkl.load(open(f"training_data/optim_{custom_single_tasks[i]}_multi_static.pkl","rb"))
+        config[0].descriptor_set.reload_descriptors()
+        all_desc[custom_single_tasks[i]]=(config[0])
+    pkl.dump(all_desc, open(f"training_data/optim_multi_static.pkl","wb"))
+
+    for i in range(len(custom_single_tasks)):
+        optim_config = optimize_configurations(custom_single_tasks[i],4,6, filter_fn=lambda name:"_1" in name[0], name="hand", label_name="labels_hand.yml")
+        pkl.dump(optim_config, open(f"training_data/optim_{custom_single_tasks[i]}_multi_hand.pkl","wb"))
+    all_desc = {}
+    for i in range(len(custom_single_tasks)):
+        config = pkl.load(open(f"training_data/optim_{custom_single_tasks[i]}_multi_hand.pkl","rb"))
         config = sorted(config, reverse=True, key=lambda c:c.f1)
         config[0].descriptor_set.reload_descriptors()
         all_desc[custom_single_tasks[i]]=(config[0])
-    pkl.dump(all_desc, open(f"training_data/optim_hand.pkl","wb"))
-
-    for i in range(len(custom_single_tasks)):
-        optim_config = optimize_configurations(custom_single_tasks[i],6,6, filter_fn=lambda name:"_0" in name[0], name="static")
-        pkl.dump(optim_config, open(f"training_data/optim_{custom_single_tasks[i]}_static.pkl","wb"))
-    all_desc = {}
-    for i in range(len(custom_single_tasks)):
-        config = pkl.load(open(f"training_data/optim_{custom_single_tasks[i]}_static.pkl","rb"))
-        config[0].descriptor_set.reload_descriptors()
-        all_desc[custom_single_tasks[i]]=(config[0])
-    pkl.dump(all_desc, open(f"training_data/optim_static.pkl","wb"))
+    pkl.dump(all_desc, open(f"training_data/optim_multi_hand.pkl","wb"))
